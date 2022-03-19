ed254d8a2b28fc64f590bf925e7bbd6fae77de3d
Yuriy <yuriy.sam2002@gmail.com>
2022-03-20 00:48:38 +0100

    first commit ready

---
+++ script.js
@@ -0,0 +1,34 @@
+"use sctrict"
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+// number = 10;
+
+// console.log(number);
+// const persone = "Alex";
+// const obj = {
+//     name: "john",
+//     age: 25,
+
+// }
+// console.log(obj.name);
+// alert ("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = +prompt("Сколько вам лет?", "10");
+// console.log(answer + 5);
+
+// const answers = [];
+// answers[0] = prompt("Ваше имя?", "");
+// answers[1] = prompt("Ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// console.log(typeof(answers));
+
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);

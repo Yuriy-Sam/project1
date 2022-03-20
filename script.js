c303744720b88521de270e1cc82bdeb80d796712
Yuriy <yuriy.sam2002@gmail.com>
2022-03-20 12:16:40 +0100

    DZ practic1

--- script.js
+++ script.js
@@ -32,3 +32,27 @@
 // decr--;
 // console.log(incr);
 // console.log(decr);
+
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+}
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
